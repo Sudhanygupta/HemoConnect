40b1624a79c7c6e875e294f127222bc081003444
Ahaan25 <ahaan25092002@gmail.com>
2022-04-19 01:25:55 +0530

    hehehehe

--- app/src/main/java/com/example/blooddonation/ViewRecords.java
+++ app/src/main/java/com/example/blooddonation/ViewRecords.java
@@ -3,12 +3,24 @@ package com.example.blooddonation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ViewRecords extends AppCompatActivity {
 
+    Button singlerec, allrecs;
+    EditText id;
+    TextView srec, allrec;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_records);
+
+        srec=findViewById(R.id.textView);
+        allrec=findViewById(R.id.textView2);
+        id=findViewById(R.id.editTextTextPersonName);
+        singlerec=findViewById(R.id.button4);
+        allrecs=findViewById(R.id.button5);
     }
 }
