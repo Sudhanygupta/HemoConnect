946737c4c2df9c89de2e42ea8de27582715de223
Ahaan25 <ahaan25092002@gmail.com>
2022-04-19 16:53:54 +0530

    hehehehe

--- app/src/main/java/com/example/blooddonation/LoginActivity.java
+++ app/src/main/java/com/example/blooddonation/LoginActivity.java
@@ -2,9 +2,12 @@ package com.example.blooddonation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -18,5 +21,31 @@ public class LoginActivity extends AppCompatActivity {
         login=findViewById(R.id.button8);
         un=findViewById(R.id.editTextTextPersonName9);
         pass=findViewById(R.id.editTextTextPersonName10);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                if (un.getText().toString().equals("admin")&&pass.getText().toString().equals("admin")){
+                    openMainActivity();
+                    Toast.makeText(getApplicationContext(),"Welcome",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    int count=3;
+                    Toast.makeText(getApplicationContext(),"Wrong credentials",Toast.LENGTH_SHORT).show();
+                    count--;
+                    un.setText(null);
+                    pass.setText(null);
+                    if(count==0){
+                        login.setEnabled(false);
+                    }
+                }
+            }
+        });
+    }
+
+    public void openMainActivity(){
+        Intent i=new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
