4b66e6af8cc865d9c16d3f65a33be5afb5322571
Ahaan25 <ahaan25092002@gmail.com>
2022-04-19 21:15:43 +0530

    hehehehe

--- app/src/main/java/com/example/blooddonation/LoginActiviy.java
+++ app/src/main/java/com/example/blooddonation/LoginActiviy.java
@@ -2,9 +2,12 @@ package com.example.blooddonation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActiviy extends AppCompatActivity {
 
@@ -17,5 +20,34 @@ public class LoginActiviy extends AppCompatActivity {
         un=findViewById(R.id.editTextTextPersonName12);
         pass=findViewById(R.id.editTextTextPersonName13);
         login=findViewById(R.id.button10);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (un.getText().toString().equals("root")&&pass.getText().toString().equals("vpms")){
+                    openMainActivity();
+                    Toast.makeText(getApplicationContext(),"Welcome",Toast.LENGTH_SHORT).show();
+                }
+                /*
+                else if (un.getText().toString().equals(null)||pass.getText().toString().equals(null)){
+                    Toast.makeText(getApplicationContext(),"Please enter credentials",Toast.LENGTH_SHORT).show();
+                }
+                */
+                else{
+                    int count=3;
+                    count--;
+                    Toast.makeText(getApplicationContext(),"Invalid credentials",Toast.LENGTH_SHORT).show();
+                    un.setText(null);
+                    pass.setText(null);
+                    if(count==0){
+                        login.setClickable(false);
+                    }
+                }
+            }
+        });
+    }
+
+    public void openMainActivity(){
+        Intent i=new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/blooddonation/SignupActivity.java
+++ app/src/main/java/com/example/blooddonation/SignupActivity.java
@@ -22,34 +22,20 @@ public class SignupActivity extends AppCompatActivity {
         un=findViewById(R.id.editTextTextPersonName9);
         pass=findViewById(R.id.editTextTextPersonName10);
         repass=findViewById(R.id.editTextTextPersonName11);
+
+
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (un.getText().toString().equals("root")&&pass.getText().toString().equals("vpms")){
-                    openMainActivity();
-                    Toast.makeText(getApplicationContext(),"Welcome",Toast.LENGTH_SHORT).show();
-                }
-                /*
-                else if (un.getText().toString().equals(null)||pass.getText().toString().equals(null)){
-                    Toast.makeText(getApplicationContext(),"Please enter credentials",Toast.LENGTH_SHORT).show();
-                }
-                */
-                else{
-                    int count=3;
-                    count--;
-                    Toast.makeText(getApplicationContext(),"Invalid credentials",Toast.LENGTH_SHORT).show();
-                    un.setText(null);
-                    pass.setText(null);
-                    if(count==0){
-                        login.setClickable(false);
-                    }
-                }
+                loginActivity();
             }
         });
+
     }
 
-    public void openMainActivity(){
-        Intent i=new Intent(this, MainActivity.class);
+    public void loginActivity(){
+        Intent i=new Intent(this, LoginActiviy.class);
         startActivity(i);
     }
 }
