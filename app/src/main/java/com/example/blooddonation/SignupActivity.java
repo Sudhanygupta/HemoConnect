786f0f8bab53b74572b40c93292abc62e02721ba
Ahaan25 <ahaan25092002@gmail.com>
2022-04-21 14:24:21 +0530

    hehehehe

--- app/src/main/java/com/example/blooddonation/SignupActivity.java
+++ app/src/main/java/com/example/blooddonation/SignupActivity.java
@@ -9,21 +9,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.regex.Pattern;
+
 public class SignupActivity extends AppCompatActivity {
 
     EditText un, pass, repass;
-    Button signup;
+    Button signup, back;
     DBhelper db;
 
-    /*
-    private static final Pattern PASSWORD_PATTERN =
-            Pattern.compile("^" +
-                    "(?=.*[@#$%^&+=])" +
-                    "(?=\\S+$)" +
-                    ".{4,}" +
-                    "$");
+    private static final Pattern passmin=Pattern.compile("^"+"(?=.*[@#$%^&+=])"+"(?=\\S+$)"+".{4,}"+"$");
 
-     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,27 +26,11 @@ public class SignupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         signup=findViewById(R.id.button9);
+        back=findViewById(R.id.button13);
         un=findViewById(R.id.editTextTextPersonName9);
         pass=findViewById(R.id.editTextTextPersonName10);
         repass=findViewById(R.id.editTextTextPersonName11);
         db=new DBhelper(this);
-
-        /*
-        private boolean validatePassword() {
-            String passwordInput = pass.getText().toString().trim();
-            if (passwordInput.isEmpty()) {
-                pass.setError("Field can not be empty");
-                return false;
-            }
-            else if (!PASSWORD_PATTERN.matcher(passwordInput).matches()) {
-                pass.setError("Password is too weak");
-                return false;
-            } else {
-                pass.setError(null);
-                return true;
-            }
-        }
-         */
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,7 +46,7 @@ public class SignupActivity extends AppCompatActivity {
                         Boolean checkuser=db.check(username);
                         if(checkuser==false){
                             Boolean insert=db.insert(username, password);
-                            if(insert==true) {
+                            if(insert==true){
                                 Toast.makeText(SignupActivity.this, "Registered.", Toast.LENGTH_SHORT).show();
                                 Intent i=new Intent(getApplicationContext(), MainActivity.class);
                                 startActivity(i);
@@ -84,9 +63,42 @@ public class SignupActivity extends AppCompatActivity {
                         Toast.makeText(SignupActivity.this, "Passwords do not match.", Toast.LENGTH_SHORT).show();
                     }
                 }
+                enterallfields();
+                validatePassword();
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 loginActivity();
             }
         });
+
+    }
+
+    private boolean validatePassword() {
+        String password=pass.getText().toString().trim();
+        if (password.isEmpty()) {
+            pass.setError("Field can not be empty");
+            return false;
+        }
+        else if (!passmin.matcher(password).matches()) {
+            pass.setError("Password does not meet minimum requirements");
+            return false;
+        } else {
+            pass.setError(null);
+            return true;
+        }
+    }
+
+    public void enterallfields(){
+        String a=un.getText().toString();
+        String b=pass.getText().toString();
+        String c=repass.getText().toString();
+        if(a.equals(null)||b.equals(null)||c.equals(null)){
+            signup.setClickable(false);
+        }
     }
 
     public void loginActivity(){
