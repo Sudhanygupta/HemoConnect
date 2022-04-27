3b5fb090def64a2e8b3eb27b9ccb93b07f6acff5
Ahaan25 <ahaan25092002@gmail.com>
2022-04-27 21:12:36 +0530

    hehehehe

--- app/src/main/java/com/example/blooddonation/MapsActivity.java
+++ app/src/main/java/com/example/blooddonation/MapsActivity.java
@@ -3,7 +3,10 @@ package com.example.blooddonation;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -14,10 +17,21 @@ import com.google.android.gms.maps.model.LatLng;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
     GoogleMap map;
+    Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        back=findViewById(R.id.button26);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ab=new Intent(MapsActivity.this, CheckAvail.class);
+                startActivity(ab);
+            }
+        });
+
         SupportMapFragment fm =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         fm.getMapAsync(MapsActivity.this);
