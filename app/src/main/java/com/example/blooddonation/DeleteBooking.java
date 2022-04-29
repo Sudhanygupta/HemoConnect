01fbd1939dc696842cffce412aaab5ea7f068d7d
Ahaan25 <ahaan25092002@gmail.com>
2022-04-29 11:29:49 +0530

    hehehehe

--- app/src/main/java/com/example/blooddonation/DeleteBooking.java
+++ app/src/main/java/com/example/blooddonation/DeleteBooking.java
@@ -90,9 +90,6 @@ public class DeleteBooking extends AppCompatActivity {
                 intent.setType("message/rfc822");
                 startActivity(Intent.createChooser(intent, "Choose an Email client :"));
 
-                //Intent ab=new Intent(DeleteBooking.this, BookAppointment.class);
-                //startActivity(ab);
-
             }
         });
 
