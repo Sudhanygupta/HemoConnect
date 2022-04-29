59dae2d29a2d3cfda334422eecea11f8d30151a4
Ahaan25 <ahaan25092002@gmail.com>
2022-04-29 18:56:13 +0530

    hehehehe

--- app/src/main/java/com/example/blooddonation/CheckAvail.java
+++ app/src/main/java/com/example/blooddonation/CheckAvail.java
@@ -20,43 +20,25 @@ import java.util.Calendar;
 public class CheckAvail extends AppCompatActivity  {
 
     TextView anim;
-    Button check, call, book, back;
+    Button check, call, book, back, test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_avail);
 
-        check=findViewById(R.id.button16);
-        call=findViewById(R.id.button17);
+        check=findViewById(R.id.button28);
         book=findViewById(R.id.button18);
+        call=findViewById(R.id.button17);
+        anim=findViewById(R.id.textView4);
         back=findViewById(R.id.button27);
 
-        anim=findViewById(R.id.textView4);
-        anim.setEllipsize(TextUtils.TruncateAt.MARQUEE);
-        anim.setSelected(true);
-        anim.setSingleLine(true);
-        anim.setOnClickListener(new View.OnClickListener() {
+        test=findViewById(R.id.button500);
+        test.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Uri uri=Uri.parse("http://nbtc.naco.gov.in/");
-                Intent intent=new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
-            }
-        });
-
-        /*
-        Spinner bloodtypes=findViewById(R.id.spinner);
-        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.bloodtypes, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        bloodtypes.setAdapter(adapter);
-        */
-
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent ab=new Intent(CheckAvail.this, MainActivity.class);
-                startActivity(ab);
+                Intent abc=new Intent(CheckAvail.this, TestActivity.class);
+                startActivity(abc);
             }
         });
 
@@ -67,7 +49,6 @@ public class CheckAvail extends AppCompatActivity  {
                 startActivity(s);
             }
         });
-
 
         book.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,6 +64,26 @@ public class CheckAvail extends AppCompatActivity  {
                 Intent intent=new Intent(Intent.ACTION_DIAL);
                 intent.setData(Uri.parse("tel:104"));
                 startActivity(intent);
+            }
+        });
+
+        anim.setEllipsize(TextUtils.TruncateAt.MARQUEE);
+        anim.setSelected(true);
+        anim.setSingleLine(true);
+        anim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri=Uri.parse("http://nbtc.naco.gov.in/");
+                Intent intent=new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ab=new Intent(CheckAvail.this, MainActivity.class);
+                startActivity(ab);
             }
         });
 
