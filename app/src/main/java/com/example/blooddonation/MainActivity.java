0d8d0094e1653051fd93280e1301132138bafc9b
Ahaan25 <ahaan25092002@gmail.com>
2022-04-30 16:45:17 +0530

    hehehehe

--- app/src/main/java/com/example/blooddonation/MainActivity.java
+++ app/src/main/java/com/example/blooddonation/MainActivity.java
@@ -10,13 +10,13 @@ import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button viewrecords, addrecords, deleterecords, editrecords, checkavail;
+    //Button viewrecords, addrecords, deleterecords, editrecords, checkavail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        /*
         viewrecords=findViewById(R.id.button);
         addrecords=findViewById(R.id.button2);
         deleterecords=findViewById(R.id.button3);
@@ -62,7 +62,39 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(ab);
             }
         });
-
+        */
     }
+
+    //menu code start
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                Intent i=new Intent(MainActivity.this, AddRecords.class);
+                startActivity(i);
+                return true;
+            case R.id.item2:
+                Intent a=new Intent(MainActivity.this, EditRecords.class);
+                startActivity(a);
+                return true;
+            case R.id.item3:
+                Intent ib=new Intent(MainActivity.this, DeleteRecords.class);
+                startActivity(ib);
+                return true;
+            case R.id.item4:
+                Intent ia=new Intent(MainActivity.this, ViewRecords.class);
+                startActivity(ia);
+                return true;
+            default:return super.onOptionsItemSelected(item);
+        }
+    }
+    //menu code end
 
 }
